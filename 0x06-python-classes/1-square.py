84c978198b94975374e0be605f3f5416ab570f16
Joseph-muigai <wambichojoseph@gmail.com>
2022-06-22 11:49:31 +0300

    add  1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """
+        size - the size of the square 
+        """
+        self.__size = size
+
