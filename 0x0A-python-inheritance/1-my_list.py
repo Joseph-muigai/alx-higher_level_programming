ef0bd87d2c02b83c97939a23cb97453b3bf6e3a2
Joseph-muigai <wambichojoseph@gmail.com>
2022-07-04 21:49:58 +0300

    added task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class MyList thta inherits class List"""
+
+class MyList(List):
+    """imprements sorted printing"""
+    def print_sorted(self):
+        """Printd a list sorted in ascending order"""
+        print(sorted(self))
+
