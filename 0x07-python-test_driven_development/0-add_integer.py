4f52f17dceb2f164db7014183539371c78a7023b
Joseph-muigai <wambichojoseph@gmail.com>
2022-07-08 12:18:54 +0300

    make some changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,9 +7,9 @@ def add_integer(a, b=98):
     a funtion that adds 2 intergers
     a and b must be intergers otherwise throw a typeError
     """
-    if isinstance(a,int) and isinstance(a,float):
+    if isinstance(a,int) is False  and isinstance(a,float)is False :
         raise TypeError("a must be an integer")
-    if isinstance(b,int) and isinstance(b,float):
+    if isinstance(b,int) is False and isinstance(b,float)is False :
         raise TypeError("b must be an integer")
     
     return( int(a) + int(b))
