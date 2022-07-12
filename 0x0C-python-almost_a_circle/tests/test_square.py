65bd5d03f611e6b069c2488918d459302dab17af
Joseph-muigai <wambichojoseph@gmail.com>
2022-07-12 16:49:28 +0300

    add Square class tests: square is instance of Base,Rectangle

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Test cases for the Square class.
+"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+class Test_Square(unittest.TestCase):
+    """unitTests for testing the Square class"""
+
+    def setUp(self) -> None:
+        """Method invoked for each test"""
+        Base._Base__nb_objects = 0
+        self.s1 = Square(4)
+    
+    def test_new_square(self):
+        """Test new square """
+        self.assertEqual(self.s1.id, 1)
+    
+    def test_is_Base_instance(self):
+        """Check if square is an instance og Base"""
+        self.assertEqual(isinstance(self.s1, Base), True)
+    
+    def test_is_Rectangle_instance(self):
+        """Check if Square is an instance of Rectangle"""
