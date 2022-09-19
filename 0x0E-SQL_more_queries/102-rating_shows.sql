1f69e00158d860b5fe6f63c495b118b7c1bc54e6
Joseph-muigai <wambichojoseph@gmail.com>
2022-09-19 21:27:13 +0300

    a script that lists all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- a script that lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(tv_shows_rating) AS 'rating'
+SELECT tv_shows.title, SUM(tv_shows_ratings.rate) AS 'rating'
 FROM tv_shows
-INNER JOIN tv_shows_rating
-ON tv_shows.id = tv_shows_rating.show_id
+INNER JOIN tv_shows_ratings
+ON tv_shows.id = tv_shows_ratings.show_id
 GROUP BY title
 ORDER BY rating DESC;
