f1f0a2cc15bf1b7583bbdac315d0de77c0ca948a
Joseph-muigai <wambichojoseph@gmail.com>
2022-09-19 16:32:59 +0300

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,6 +4,6 @@
 
 CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
 CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
