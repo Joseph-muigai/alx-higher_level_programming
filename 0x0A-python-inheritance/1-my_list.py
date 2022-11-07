529339289cf185e23d4c85f04f5181b59cbbcfd3
Joseph Muigai <68539624+Joseph-muigai@users.noreply.github.com>
2022-11-07 21:52:38 +0300

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines a class MyList thta inherits class List"""
-
-class MyList(List):
-    """imprements sorted printing"""
-    def print_sorted(self):
-        """Printd a list sorted in ascending order"""
-        print(sorted(self))
